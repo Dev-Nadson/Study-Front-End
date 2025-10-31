dbba92e1456015d402ece0b70734eb190ca3873a
Dev-Nadson <nadson.pros@gmail.com>
2025-10-31 14:47:11 -0300

    Página estilizada

--- Counter-Project/src/App.tsx
+++ Counter-Project/src/App.tsx
@@ -1,9 +1,12 @@
 import { Button } from "./components/button"
 
+let num = 0
+
 function App() {
   return <>
-    <div className="bg-slate-900 h-screen">
+    <div className="bg-slate-250 dark:bg-slate-900 h-screen flex items-center justify-center ">
       <Button name="Adicionar" />
+      <p className="text-stone-900 dark:text-amber-50 text-6xl font-serif">{num}</p>
       <Button name="Remover" />
     </div>
   </>
--- Counter-Project/src/components/button.tsx
+++ Counter-Project/src/components/button.tsx
@@ -3,7 +3,7 @@ type buttonProps = {
 }
 
 function Button({ name }: buttonProps) {
-    return <button className="bg-indigo-600 px-4 py-2 rounded-xl mx-10 my-6 text-amber-50 font-semibold border-4 border-indigo-700 hover:bg-indigo-700 hover:border-indigo-800">{name}</button>
+    return <button className="bg-indigo-600 px-4 py-2 rounded-xl mx-8 text-amber-50 font-semibold border-4 border-indigo-700 shadow-[0_3px_100px] shadow-blue-600 hover:bg-indigo-700 hover:border-indigo-800 hover:ring dark:ring-amber-50 ring-gray-900">{name}</button>
 }
 
 export { Button }
